ca8502271c35aeb4d8f9362ccf5b8973b2217b20
jnovikov <johnnovikov0@gmail.com>
2019-11-17 13:53:02 +0300

    Bydlofix

--- services/anilist/src/anidb/app/Http/Controllers/AnimeController.php
+++ services/anilist/src/anidb/app/Http/Controllers/AnimeController.php
@@ -17,6 +17,19 @@ class AnimeController extends Controller
         $this->animeService = $service;
     }
 
+    public function getUserId(Request $request)
+    {
+        $sessionId = $request->cookie('session');
+        if ($sessionId && $sessionId != '') {
+            $sessionUser = app('redis')->get($sessionId);
+            $user = json_decode($sessionUser, true);
+            if ($user && $user['id']) {
+                return $user["id"];
+            }
+        }
+        return null;
+    }
+
     public function addAnime(Request $request)
     {
         try {
@@ -31,20 +44,18 @@ class AnimeController extends Controller
             return response()->json(["error" => $e], 422);
         }
 
-        $user = $request->user();
         $title = $request->input('title');
         $description = $request->input('description');
         $year = $request->input('year') ?? 0;
         $public = $request->input('public');
 
-        $ok = $this->animeService->addAnime($user->id, $title, $description, $public, $year);
+        $ok = $this->animeService->addAnime($this->getUserId($request), $title, $description, $public, $year);
 
         return response()->json(["result" => $ok]);
     }
 
     public function addAnimeLink(Request $request, $animeId)
     {
-        $user = $request->user();
 
         $anime = $this->animeService->getAnimeById($animeId);
 
@@ -52,7 +63,7 @@ class AnimeController extends Controller
             return response()->json(["error" => 'not found'], 404);
         }
 
-        if ($anime->owner_id != $user->id) {
+        if ($anime->owner_id != $this->getUserId($request)) {
             return response()->json(["error" => 'Unauthorized'], 403);
         }
 
@@ -64,9 +75,7 @@ class AnimeController extends Controller
 
     public function listMyAnime(Request $request)
     {
-        $user = $request->user();
-
-        return response()->json(["result" => $this->animeService->getMyAnime($user->id)]);
+        return response()->json(["result" => $this->animeService->getMyAnime($this->getUserId($request))]);
     }
 
     public function listAnime(Request $request)
@@ -81,7 +90,7 @@ class AnimeController extends Controller
             $animeId = $request->input('anime');
             $ok = $this->animeService->tryGetAnimeAccess($animeId, $request->input('token'));
             if ($ok) {
-                return response()->json(["result" => $this->animeService->addAnimeAccess($animeId, $request->user()->id)]);
+                return response()->json(["result" => $this->animeService->addAnimeAccess($animeId, $this->getUserId($request))]);
             }
         }
         return response()->json(["error" => "not ok"]);
@@ -89,7 +98,7 @@ class AnimeController extends Controller
 
     public function detailsAnime(Request $request, $animeId)
     {
-        $details = $this->animeService->getAnimeDetails($animeId, $request->user()->id);
+        $details = $this->animeService->getAnimeDetails($animeId, $this->getUserId($request));
         if ($details) {
             return response()->json(["result" => $details]);
         }
